c67c536ea383917fd6c9820e6b66dafa1599f2b5
Travis Donia <tdonia@gmail.com>
2023-05-22 13:19:11 -0400

    fix tailwind pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.js', './src/**/*.jsx', '../modelmodel/src/**/*.jsx', './node_modules/modelmodel/src/**/*.{jsx}'],
+  content: ['./src/**/*.js', './src/**/*.jsx', '../modelmodel/src/**/*.jsx', './node_modules/modelmodel/src/**/*.jsx'],
   theme: {
     extend: {},
   },
